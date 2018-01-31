e99880c824b6484c686af4fe8592339058eed388
Quoc Minh VU <minhvq27@gmail.com>
2018-01-31 14:49:47 -0500

    Convert cell letter to use character instead of enum Add function to get cell index by cell char and vice versa

--- src/model/Config.java
+++ src/model/Config.java
@@ -8,72 +8,81 @@ public class Config {
         private static final String INVALID_CELLS = "Cells letter range from A to O";
         private static final String INVALID_INDEX = "Cells index range from 0 to 14";
         
-        public enum CELLS {
-            A(0), B(1), C(2), D(3), E(4), F(5), G(6), H(7), I(8), J(9), K(10), L(11), M(12), N(13), O(14);
-            
-            private final int index;
-            
-            CELLS(int index) {
-                this.index = index;
+        public char getCellCharByIndex(int cellIndex) {
+            if (cellIndex < 0 || cellIndex > 14) {
+                throw new IllegalArgumentException(INVALID_INDEX);
             }
             
-            public int getIndex() {
-                return index;
-            }
+            return (char) (cellIndex + 65);
         }
         
-        public ArrayList<CELLS> getCellsMovableTo(CELLS emptyCell) {
-            CELLS cells[];
-            switch (emptyCell) {
-                case A:
-                    cells = new CELLS[] { CELLS.B, CELLS.F };
+        public int getCellIndexByChar(char cellChar) {
+            cellChar = Character.toUpperCase(cellChar);
+            int cellIndex = (int) cellChar - 65;
+            if (cellIndex < 0 || cellIndex > 14) {
+                throw new IllegalArgumentException(INVALID_CELLS);
+            }
+            return cellIndex;
+        }
+        
+        public ArrayList<Character> getCellsMovableTo(char cellChar) {
+            cellChar = Character.toUpperCase(cellChar);
+            Character cells[];
+            switch (cellChar) {
+                case 'A':
+                    cells = new Character[] {'B', 'F'};
                     break;
-                case B:
-                    cells = new CELLS[] { CELLS.A, CELLS.G, CELLS.C };
+                case 'B':
+                    cells = new Character[] {'A', 'G', 'C'};
                     break;
-                case C:
-                    cells = new CELLS[] { CELLS.B, CELLS.H, CELLS.D };
+                case 'C':
+                    cells = new Character[] {'B', 'H', 'D'};
                     break;
-                case D:
-                    cells = new CELLS[] { CELLS.C, CELLS.I, CELLS.E };
+                case 'D':
+                    cells = new Character[] {'C', 'I', 'E'};
                     break;
-                case E:
-                    cells = new CELLS[] { CELLS.D, CELLS.J };
+                case 'E':
+                    cells = new Character[] {'D', 'J'};
                     break;
-                case F:
-                    cells = new CELLS[] { CELLS.A, CELLS.G, CELLS.K };
+                case 'F':
+                    cells = new Character[] {'A', 'G', 'K'};
                     break;
-                case G:
-                    cells = new CELLS[] { CELLS.B, CELLS.F, CELLS.L, CELLS.H };
+                case 'G':
+                    cells = new Character[] {'B', 'F', 'L', 'H'};
                     break;
-                case H:
-                    cells = new CELLS[] { CELLS.C, CELLS.G, CELLS.M, CELLS.I };
+                case 'H':
+                    cells = new Character[] {'C', 'G', 'M', 'I'};
                     break;
-                case I:
-                    cells = new CELLS[] { CELLS.D, CELLS.H, CELLS.N, CELLS.J };
+                case 'I':
+                    cells = new Character[] {'D', 'H', 'N', 'J'};
                     break;
-                case J:
-                    cells = new CELLS[] { CELLS.E, CELLS.I, CELLS.O };
+                case 'J':
+                    cells = new Character[] {'E', 'I', 'O'};
                     break;
-                case K:
-                    cells = new CELLS[] { CELLS.F, CELLS.L };
+                case 'K':
+                    cells = new Character[] {'F', 'L'};
                     break;
-                case L:
-                    cells = new CELLS[] { CELLS.K, CELLS.G, CELLS.M };
+                case 'L':
+                    cells = new Character[] {'K', 'G', 'M'};
                     break;
-                case M:
-                    cells = new CELLS[] { CELLS.L, CELLS.H, CELLS.N };
+                case 'M':
+                    cells = new Character[] {'L', 'H', 'N'};
                     break;
-                case N:
-                    cells = new CELLS[] { CELLS.M, CELLS.I, CELLS.O };
+                case 'N':
+                    cells = new Character[] {'M', 'I', 'O'};
                     break;
-                case O:
-                    cells = new CELLS[] { CELLS.N, CELLS.J };
+                case 'O':
+                    cells = new Character[] {'N', 'J'};
                     break;
                 default:
                     throw new IllegalArgumentException(INVALID_CELLS);
             }
             return new ArrayList<>(Arrays.asList(cells));
+        }
+    
+        public ArrayList<Character> getCellsMovableTo(int cellIndex) {
+            char cellChar = getCellCharByIndex(cellIndex);
+            return getCellsMovableTo(cellChar);
         }
     }
 }
